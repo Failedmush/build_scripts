e3c37aa4eca712befa230a3976a96557c173375b
Failedmush <163705497+Failedmush@users.noreply.github.com>
2025-06-30 06:29:23 +0900

    Update Axion.sh

--- Axion.sh
+++ Axion.sh
@@ -1,4 +1,4 @@
-"rm -rf .repo/local_manifests && \
+rm -rf .repo/local_manifests && \
 rm -rf {device,vendor,kernel,hardware}/xiaomi && \ 
 repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android-b lineage-22.2 -g default,-mips,-darwin,-notdefault && \
 /opt/crave/resync.sh && \
